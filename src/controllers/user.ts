4debae6cdb03ea0c382c5051f9b4236fce41206d
Chris Lovell <clovellbsc@gmail.com>
2022-09-21 16:38:28 +0100

    user controller added with bcrypt authentication

---
+++ src/controllers/user.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from "express";
+import catchBlock from "../utils/catchBlock";
+import userSchema from "../models/user";
+import bcrypt from "bcrypt";
+
+const User = {
+  get: async (req: Request, res: Response) => {
+    try {
+      await userSchema.find({}).then((users) => res.send(users));
+    } catch (e: unknown) {
+      res.send(e);
+    }
+  },
+  Register: async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await userSchema.create({
+        username: username,
+        email: email,
+        password: hashedPassword,
+      });
+      res.status(200).send("User added successfully");
+    } catch (e: unknown) {
+      catchBlock(e, res);
+    }
+  },
+  Login: async (req: Request, res: Response) => {
+    const { username, email, password } = req.body;
+    const foundUser = await userSchema.findOne({ username: req.body.username });
+    if (foundUser === null) {
+      return res.status(400).send();
+    }
+    try {
+      if (await bcrypt.compare(password, foundUser.password)) {
+        res.send("Success");
+      } else {
+        res.send("Incorrect email or password");
+      }
+      res.status(200).send(foundUser);
+    } catch (e: unknown) {
+      catchBlock(e, res);
+    }
+  },
+};
+
+export default User;
