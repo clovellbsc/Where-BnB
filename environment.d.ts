9c9c8515402437d579c67e3cd54884160e50b761
Chris Lovell <clovellbsc@gmail.com>
2022-09-27 12:01:39 +0100

    updated environment.d.ts for aws variables

--- environment.d.ts
+++ environment.d.ts
@@ -5,10 +5,14 @@ export {};
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PORT: String;
-      ATLAS_URI: String;
+      PORT: string;
+      ATLAS_URI: string;
       ACCESS_TOKEN: Secret;
       REFRESH_TOKEN: Secret;
+      AWS_ACCESS_KEY_ID: Secret;
+      AWS_SECRET_ACCESS_KEY: Secret;
+      AWS_REGION: string;
+      AWS_BUCKET_NAME: string;
     }
   }
 }
