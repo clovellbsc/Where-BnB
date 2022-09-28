65c447860a9d9c7c839656bc280d82404be2a47e
Chris Lovell <clovellbsc@gmail.com>
2022-09-28 13:30:46 +0100

    controller for getting all acommodation
    limit of 50 results returned

--- src/controllers/accommodation.ts
+++ src/controllers/accommodation.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from "express";
 import catchBlock from "../utils/catchBlock";
+import accommodationSchema from "../models/accommodation";
 
 const AccommodationController = {
   All: async (req: Request, res: Response) => {
     try {
-      res.send("successful");
+      const accomodationArray = accommodationSchema.find().limit(50);
+
+      res
+        .status(200)
+        .send({ success: "Successful", accommodation: accomodationArray });
     } catch (e: unknown) {
       catchBlock(e, res);
     }
