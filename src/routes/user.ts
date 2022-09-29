97f95111ced10bc906a8b03d33550c8d46af912b
Chris Lovell <clovellbsc@gmail.com>
2022-09-29 19:13:07 +0100

    avatar upload route created

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import UserController from "../controllers/user";
 import { auth } from "../utils/auth";
+import { upload } from "../utils/multer";
 const userRouter = express.Router();
 
 userRouter.post("/login", UserController.Login);
 userRouter.post("/register", UserController.Register);
 userRouter.post("/delete", auth, UserController.Delete);
 userRouter.get("/", auth, UserController.get);
+userRouter.post(
+  "/upload",
+  upload.array("file", 1),
+  auth,
+  UserController.UploadAvatar
+);
 
 export default userRouter;
