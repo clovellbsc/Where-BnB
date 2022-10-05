0a6171e4f2e5156e41d92b898f9653e5855ff9a0
Chris Lovell <clovellbsc@gmail.com>
2022-10-05 17:38:19 +0100

    nodemailer transporter and sendMail function created

---
+++ src/utils/nodemailer.ts
@@ -0,0 +1,33 @@
+import nodemailer from "nodemailer";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+interface mailOptions {
+  from: string;
+  to: string;
+  subject: string;
+  html: string;
+}
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.GOOGLE_GENERATED_PASSWORD,
+  },
+});
+
+transporter.verify((err, success) => {
+  err
+    ? console.log(err)
+    : console.log(`=== Server is ready to take messages: ${success} ===`);
+});
+
+export const sendMail = (mailOptions: mailOptions) =>
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("Error " + err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
