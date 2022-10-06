151e4e1872f329d0c0dfa6daa2f65f3d6dbbd8d3
Chris Lovell <clovellbsc@gmail.com>
2022-10-06 11:25:56 +0100

    moving of server files to dedicated server folder
    This is part of the process of developing to a more fullstack applciation.
    A client folder will also be created.

--- server/src/models/accommodation.ts
+++ server/src/models/accommodation.ts
@@ -13,28 +13,29 @@ interface IAccommodation {
 const AccommodationSchema = new Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Add a name for your new listing."],
   },
   location: {
     type: String,
-    required: true,
+    required: [true, "Please add a location for your new listing."],
   },
   photos: [{ type: String }],
   pricePerNight: {
     type: Number,
-    required: true,
+    required: [true, "Please add a Price per night for your new listing."],
   },
   noOfGuests: {
     type: Number,
-    required: true,
+    required: [true, "Please state how many guests your new listing can hold."],
   },
   noOfBaths: {
     type: Number,
-    required: true,
+    required: [true, "Please state how many bathrooms your new listing has."],
   },
   owner: {
     type: Schema.Types.ObjectId,
     ref: "User",
+    req: true,
   },
 });
 
